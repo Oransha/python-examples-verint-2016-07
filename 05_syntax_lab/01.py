dbdc93d1d81ac4b7504f2a01c2652eb97d3afeee
Oransha <oran.sharabani@verint.com>
2016-07-06 23:57:50 +0300

    Solution for: sol-python-verint.syntax-lab/01.py

--- 05_syntax_lab/01.py
+++ 05_syntax_lab/01.py
@@ -1,18 +1,6 @@
-# get 10 numbers and return the largest number
+print "please insert 10 numbers"
+x = []
+for i in range (10):
+    x.append (int(raw_input()))
 
-print "please enter 10 numbers"
-
-x1 = int(raw_input ())
-x2 = int(raw_input ())
-x3 = int(raw_input ())
-x4 = int(raw_input ())
-x5 = int(raw_input ())
-x6 = int(raw_input ())
-x7 = int(raw_input ())
-x8 = int(raw_input ())
-x9 = int(raw_input ())
-x10 = int(raw_input ())
-
-
-print "the largest number is: ",  max (x1,x10)
-
+print max(x)
