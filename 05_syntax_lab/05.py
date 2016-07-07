92b839d58370a3a72919582c5d2394286b758bf5
Oransha <oran.sharabani@verint.com>
2016-07-07 10:22:08 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,10 @@
-"""
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
-"""
+import random
 
+while True :
+    x = int(random.randint(1,1000001))  
+    if x % 7 == 0 and x % 13 == 0 and x % 15 == 0:
+        print "this number divisible by 7, 13 and 15:", x
+        break
+    else:
+        continue
+    
