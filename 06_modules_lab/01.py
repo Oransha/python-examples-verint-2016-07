769aa54598af36d91d223cebbeb90a1d1898c121
Oransha <oran.sharabani@verint.com>
2016-07-08 18:41:15 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,4 +1,13 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times
-"""
+import sys
+
+if len(sys.argv) != 2:
+    print "Error, please enter only one number"
+    sys.exit()
+
+num = int(sys.argv[1])
+
+for i in range (0,num):
+    print "Hello python"
+
+    
+   
