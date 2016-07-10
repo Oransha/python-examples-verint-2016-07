956d946fc49423c4269955f3922cf9eda024e511
Oransha <oran.sharabani@verint.com>
2016-07-10 15:20:32 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,40 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+import os 
+import sys
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+print "whould you like to search in this folder(select 1) or other folder(select 2)"
+select_folder = raw_input()
 
-- Take threshold and path as command line arguments
-"""
+if select_folder == "1": 
+    new_folder = sys.argv [1]
+else:
+    new_folder = '.'
+
+
+print "whould you lile to select min size?Y\N"
+select_size = raw_input()
+
+if select_size == "y":
+    min_size = sys.argv [2] 
+else:
+    min_size = 1000000
+
+
+file_size = os.path.getsize(new_folder)
+
+for root,dirs,files in os.walk(new_folder):
+    for name in files:
+        file = (root + "\\" + name)
+        if file_size > min_size:
+            print "%s/%s" % (root,name)
+            print "This file size is over 1MB, Would you like to delete this file? Y\N"   
+            user_call = raw_input() 
+            while True:
+                if user_call == "y":
+                    os.remove(file)
+                elif user_call == "n":
+                    break
+                else:
+                    print "error, your input is wrong, try again"
+                    user_call = raw_input()
+
 
