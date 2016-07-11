ab5e64406f1ec801d092e9444d49adea2a0217ba
Oransha <oran.sharabani@verint.com>
2016-07-11 09:47:16 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,12 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+import sys
+import os 
 
+(_, file1, file2) = sys.argv
+
+with open ("./" + file1, 'r') as fin:
+    with open ("./" + file2, 'w') as fout:  
+        for line in fin:
+            fout.write(line)
+            print line
+
+#should I delete file1 contents or just copy the lines to file2?
