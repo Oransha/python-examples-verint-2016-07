d2086141483231ed0f29e6707922c871ee2f8820
Oransha <oran.sharabani@verint.com>
2016-07-14 16:52:38 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,14 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
+import operator
 
+lst1 = []
+lst2 = []
+lst3 = []
+ascii = range(97, 123)
+for i in ascii:
+     lst1.append([ x for x in chr(i)])
+     lst2.append([ x for x in chr(i)])
+     lst3.append([ x for x in chr(i)])
+
+sequence = [ (x + y + z) for x in lst1 for y in lst2 for z in lst3 ]
+
+print sequence
