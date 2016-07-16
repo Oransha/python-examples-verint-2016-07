aa69fa0a7c5cbd0f4569a623d9ca6b2e26f44f7f
Oransha <oran.sharabani@verint.com>
2016-07-16 17:23:48 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,17 @@
-"""
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
-"""
+def my_sum(*numbers):
+    sum_num = 0
+    for n in numbers:
+        if type(n) is int:
+            sum_num += n
+    return sum_num
 
+print my_sum(1,2,3, "bob")
+
+def my_multi(*numbers):
+    multi_num = 1
+    for n in numbers:
+        if type(n) is int:
+            multi_num *= n
+    return multi_num
+
+print my_multi(1,2,3,4, "bob")        
