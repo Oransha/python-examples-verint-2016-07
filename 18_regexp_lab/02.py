b9df3e186fc8bf0237b5c983c4a84a59dffd3002
Oransha <oran.sharabani@verint.com>
2016-07-23 18:09:04 +0300

    Solution for: sol-python-verint.regexp-lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -1,6 +1,29 @@
-"""
-Write a function toCamelCase(text) that returns the text camel cased
-Write a function to_underscore(text) that returns the text with 
-underscores between words
-"""
 
+import re
+
+def camelCase(text):
+    res = re.search(r'(\w+)_(\w+)', text)
+    if res == None: 
+        print text, "not have _"   
+    else:
+        upper = re.sub((r'_(\w)') ,
+        lambda m: m.group(0).upper()
+        , text)
+        camel_case_word = re.sub('_' , '', upper)
+        return camel_case_word
+
+print camelCase('hi_there_hoe_r_u')
+
+def underline(text1):
+    res1 = re.search(r'([a-z])([A-Z])', text1)
+    if res1 == None:
+        print text1, "not have big letter"
+    else:
+        lower = re.sub('([A-Z])',
+        lambda n: '_' + n.group(0).lower()
+        , text1) 
+        return lower
+
+
+
+print underline('hiThereHowAre')
