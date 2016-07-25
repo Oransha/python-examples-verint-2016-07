e91b15fb18ab43f7eb1f166c430ea2b9241702a9
Oransha <oran.sharabani@verint.com>
2016-07-25 16:56:46 +0300

    Solution for: sol-python-verint.class-syntax-lab/01.py

--- 20_classes_lab/01.py
+++ 20_classes_lab/01.py
@@ -1,6 +1,22 @@
-"""
-The following code assumes a class
-named Summer exists. 
-Complete the class so correct result is printed
-"""
+class Summer(object):
+
+    def __init__(self):
+        self.items = []
+    def add(self, number):
+        self.items += [(number)]
+    def total(self):
+        return sum([i for i in self.items])
+
+s = Summer()
+t = Summer()
+
+s.add(10, 20)
+t.add(50)
+s.add(30)
+
+# should print 60
+print s.total()
+
+# should print 50
+print t.total()
 
